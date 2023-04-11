56fc989da75961803028db045fa058c8e32620a1
vincenttommi <vincenttommi@gmail.com>
2023-04-11 16:22:33 +0300

    code

---
+++ index.js
@@ -0,0 +1,29 @@
+class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`
+    }
+  }
+  
+  class Cat {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`
+    }
+  }
+  
+  class Bird {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+        return `${this.name} says squawk!`
+      }
+    }
